fe7e232b559aebde3f78afb506effeb5ba1090bd
Moinul Moin <moinulmoin101@gmail.com>
2023-11-09 15:49:43 +0600

    test: make test work

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,7 +4,7 @@ import App from "./App";
 describe("App", () => {
   it("should render", () => {
     render(<App />);
-    const heading = screen.getByText(/Vite, React, Tailwind minimal starter/i);
+    const heading = screen.getByRole("heading");
     expect(heading).toBeInTheDocument();
   });
 });
--- vite.config.ts
+++ vite.config.ts
@@ -38,6 +38,7 @@ export default defineConfig({
         "**/coverage/**",
       ],
       all: true,
+      reportOnFailure: true,
     },
   },
 });
