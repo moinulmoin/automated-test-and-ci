b1c18ad344499b2c07b53e2cd990c60f43554c4b
Moinul Moin <moinulmoin101@gmail.com>
2023-11-09 16:07:45 +0600

    feat: add vitest ui

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,7 @@ import { render, screen } from "test/utilities";
 import App from "./App";
 
 describe("App", () => {
-  it("should render", () => {
+  it("should render App component", () => {
     render(<App />);
     const heading = screen.getByRole("heading");
     expect(heading).toBeInTheDocument();
